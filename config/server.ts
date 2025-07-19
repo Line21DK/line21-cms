99814ec62cbcc98a154d569083359bfb0c3d36f1
Line21DK <din-email@example.com>
2025-07-19 12:49:51 +0200

    Fix APP_KEYS configuration for Render deployment

--- config/server.ts
+++ config/server.ts
@@ -2,7 +2,7 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env('APP_KEYS') ? env('APP_KEYS').split(',') : ['defaultKey1', 'defaultKey2'],
   },
   url: env('PUBLIC_URL', 'http://localhost:1337'),
   admin: {
