b75c263ae3f531ed36adb8034d0f0490778bac11
Line21DK <din-email@example.com>
2025-07-20 13:34:03 +0200

    Fix port configuration and add health check for Render

--- config/server.ts
+++ config/server.ts
@@ -1,10 +1,10 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 10000),
   app: {
     keys: ['defaultKey1', 'defaultKey2'],
   },
-  url: env('PUBLIC_URL', 'http://localhost:1337'),
+  url: env('PUBLIC_URL', 'http://localhost:10000'),
   admin: {
     url: env('ADMIN_URL', '/admin'),
   },
