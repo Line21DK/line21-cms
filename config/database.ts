ddc30f13ee81253522e1d74cf4c165c300275ffc
Line21DK <din-email@example.com>
2025-07-20 14:57:43 +0200

    Simplify configuration to fix 404 issues

--- config/database.ts
+++ config/database.ts
@@ -44,7 +44,7 @@ export default ({ env }) => {
     },
     sqlite: {
       connection: {
-        filename: path.join(process.cwd(), env('DATABASE_FILENAME', '.tmp/data.db')),
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
       useNullAsDefault: true,
     },
--- config/env/production/server.ts
+++ config/env/production/server.ts
@@ -5,13 +5,4 @@ export default ({ env }) => ({
     keys: ['defaultKey1', 'defaultKey2'],
   },
   url: env('PUBLIC_URL', 'https://line21-strapi-cms.onrender.com'),
-  admin: {
-    url: '/admin',
-  },
-  proxy: {
-    enabled: true,
-    ssl: true,
-    host: '0.0.0.0',
-    port: 10000,
-  },
 }); 
