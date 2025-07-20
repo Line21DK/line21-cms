7907d8ac140b4b4e34b5ab1d136432585fef4af8
Line21DK <din-email@example.com>
2025-07-20 14:10:47 +0200

    Simplify Strapi configuration to fix startup issues

--- config/env/production/server.ts
+++ config/env/production/server.ts
@@ -1,11 +1,5 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 10000),
-  app: {
-    keys: ['defaultKey1', 'defaultKey2'],
-  },
   url: env('PUBLIC_URL', 'https://line21-strapi-cms.onrender.com'),
-  admin: {
-    url: env('ADMIN_URL', '/admin'),
-  },
 }); 
--- config/server.ts
+++ config/server.ts
@@ -1,11 +1,5 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 10000),
-  app: {
-    keys: ['defaultKey1', 'defaultKey2'],
-  },
   url: env('PUBLIC_URL', 'http://localhost:10000'),
-  admin: {
-    url: env('ADMIN_URL', '/admin'),
-  },
 });
